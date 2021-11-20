b9032927f31038cd6ef55b8e800878e1e1d44e23
AlexShkreba <comulail@gmail.com>
2021-11-20 17:55:52 +0700

    Create login and users controllers for authenticating

---
+++ MobileAppAPI/MobileAppAPI/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MobileAppAPI.Models;
+
+namespace MobileAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var person = Authenticate(user);
+            if (person != null)
+            {
+                response = Ok(person);
+            }
+            return response;
+        }
+        
+        private Users Authenticate(Users user)
+        {
+            Users person = _context.users.FirstOrDefault(x => x.login == user.login && x.password == user.password);
+            return person;
+        }
+    }
+}
---
+++ MobileAppAPI/MobileAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MobileAppAPI.Models;
+
+namespace MobileAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public UsersController(Context context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost("post")]
+        public async Task<ActionResult> post(Users user)
+        {
+            if ((user == null) || (_context.users.Any(x => x.login == user.login)))
+            {
+                return BadRequest();
+            }
+            _context.users.Add(user);
+            await _context.SaveChangesAsync();
+            var tmp = await _context.users.FirstOrDefaultAsync(x => x.login == user.login);
+            var categories = await _context.categories.Where(x => x.type == "Standard").ToListAsync();
+            foreach(var i in categories)
+            {
+                _context.tasks_by_categories.Add(new Tasks_By_Categories { id_user = tmp.id, id_category = i.id, id_task = null });
+                await _context.SaveChangesAsync();
+            }
+            return Ok(user);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<ActionResult<Users>> DeleteUsers(string login)
+        {
+            var user = await _context.users.FirstOrDefaultAsync(x => x.login == login);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            foreach (var i in await _context.tasks_by_categories.Where(x => x.id_user == user.id).ToListAsync())
+            {
+                _context.tasks_by_categories.Remove(i);
+            }
+            _context.users.Remove(user);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
